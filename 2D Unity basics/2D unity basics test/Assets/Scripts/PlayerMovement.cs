b6c51269d7d134535e02f8d5afacc76e05ebca79
Maxime Bastien <113992556+Maxime122004@users.noreply.github.com>
2024-10-24 10:18:06 +0200

    Vertical movement: jumping

--- 2D Unity basics/2D unity basics test/Assets/Scripts/PlayerMovement.cs
+++ 2D Unity basics/2D unity basics test/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    private bool isGrounded = false;
 
     private Rigidbody2D rb;
 
@@ -28,9 +30,33 @@ public class PlayerMovement : MonoBehaviour
         {
             moveInput = 1f;
             Debug.Log("Right");
+
         }
 
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
+        // Jumping
+        if ((Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.Space)) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            Debug.Log("Grounded");
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = false;
+            Debug.Log("Not grounded");
+        }
     }
 }
